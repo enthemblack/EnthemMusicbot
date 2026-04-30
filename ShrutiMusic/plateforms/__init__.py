d1cd8058f4493e2b23eb9d0eeea77cf02c7ffadf
Nitish Vishwakarma <enthemblack@gmail.com>
2026-04-30 19:38:45 +0530

    𝓔𝓷𝓽𝓱𝓮𝓶𝓧_𝓜𝓾𝓼𝓲𝓬

---
+++ ShrutiMusic/plateforms/__init__.py
@@ -0,0 +1,41 @@
+# Copyright (c) 2026 Nitish Sharma <enthemblack>
+# Location: Supaul, Bihar
+#
+# All rights reserved.
+#
+# This code is the intellectual property of Nitish Sharma.
+# You are not allowed to copy, modify, redistribute, or use this
+# code for commercial or personal projects without explicit permission.
+#
+# Allowed:
+# - Forking for personal learning
+# - Submitting improvements via pull requests
+#
+# Not Allowed:
+# - Claiming this code as your own
+# - Re-uploading without credit or permission
+# - Selling or using commercially
+#
+# Contact for permissions:
+# Email: enthemblack@gmail.com
+
+
+from .Apple import AppleAPI
+from .Carbon import CarbonAPI
+from .Resso import RessoAPI
+from .Soundcloud import SoundAPI
+from .Spotify import SpotifyAPI
+from .Telegram import TeleAPI
+from .Youtube import YouTubeAPI
+
+
+# ©️ Copyright Reserved - @enthemblack  Nitish Sharma
+
+# ===========================================
+# ©️ 2025 Nand Yaduwanshi (aka @NoxxOP)
+# 🔗 GitHub : https://github.com/enthemblack/EnthemMusicbot
+# 📢 Telegram Channel : https://t.me/EnthemMusicSupport
+# ===========================================
+
+
+# ❤️ Love From ShrutiBots 
